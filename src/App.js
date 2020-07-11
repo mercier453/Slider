5b9959df4e00d5000de2c6ad9e5a67afa06bc1e0
mercier453 <61697366+mercier453@users.noreply.github.com>
2020-07-11 18:19:59 -0400

    Update App.js
    Adding commenting - removing unused state object

--- src/App.js
+++ src/App.js
@@ -13,31 +13,45 @@ class App extends Component {
     this.handleClick = this.handleClick.bind(this);
   }
   componentDidMount() {
+   // on load shuffle array
+    // create copy of state array
     let shuffledarray = [...this.state.numarray];
     for(let i = shuffledarray.length-1; i > 0; i--){
+      // set random index
       const j = Math.floor(Math.random() * i)
+      // get current item at i index
       const temp = shuffledarray[i]
+      // set i index to j index
       shuffledarray[i] = shuffledarray[j]
+      // set j index to temp held item
       shuffledarray[j] = temp;
     }
+    // get location of empty tile
     let emptyIndex = shuffledarray.indexOf('');
+    // set numarray and empty index state
     this.setState({
       emptyIndex: emptyIndex,
       numarray: shuffledarray
     })
   }
   handleClick(event) {
+    // get empty tile location
     let emptyindex = this.state.emptyIndex;
+    // get target tile index location
     let targetindex = parseInt(event.target.getAttribute('data-index'));
-    if(targetindex === emptyindex-1 || targetindex === emptyindex+1 || targetindex === emptyindex-4 || targetindex === emptyindex+4) {
+    // if target is +/- 1 or +/- grid size
+    if(targetindex === emptyindex-1 || targetindex === emptyindex+1 || targetindex === emptyindex- this.state.grid || targetindex === emptyindex + this.state.grid) {
+      // make copy of state array
       let updatedArray = [...this.state.numarray];
+      // set empty tile to target tile data
       updatedArray[emptyindex] = updatedArray[targetindex];
+      // set target to empty
       updatedArray[targetindex] = '';
-      this.setState((prevState) => ({
+      // set state for revised array and emptyindex
+      this.setState({
         numarray: updatedArray,
-        emptyIndex: targetindex,
-        changedIndex: prevState.emptyIndex
-      }))
+        emptyIndex: targetindex
+      })
     }
   }
   render() {
